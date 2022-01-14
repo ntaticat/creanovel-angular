40808e722dfe0e75ceee3b799d50d626d1ab17f4
Rafael Estrada <ntaticat@gmail.com>
2022-01-13 18:05:25 -0600

    update auth guard

--- src/app/core/guards/auth.guard.ts
+++ src/app/core/guards/auth.guard.ts
@@ -2,10 +2,10 @@ import { AppState } from '@store/app.reducer';
 import { select, Store } from '@ngrx/store';
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, CanLoad, Route, RouterStateSnapshot, UrlSegment, UrlTree } from '@angular/router';
-import { Observable, Observer, PartialObserver, Subject } from 'rxjs';
+import { Observable, Observer, PartialObserver, race, Subject, timer } from 'rxjs';
 import * as usuarioSelectors from '@store/usuarios/usuarios.selectors';
 import * as usuarioActions from '@store/usuarios/usuarios.actions';
-import { first, skip, take, takeLast, tap } from 'rxjs/operators';
+import { first, map, mapTo, skip, take, takeLast, tap } from 'rxjs/operators';
 
 
 @Injectable({
@@ -13,44 +13,53 @@ import { first, skip, take, takeLast, tap } from 'rxjs/operators';
 })
 export class AuthGuard implements CanActivate, CanLoad {
 
-  constructor(private store: Store<AppState>) {}
+  constructor(private store: Store<AppState>) { }
 
   async canLoad(route: Route, segments: UrlSegment[]) {
     const result = await this.canAccessObs();
-    console.log("-- RESULT ---> CAN LOAD", result);
     return true;
   }
 
   async canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
     const result = await this.canAccessObs();
-    console.log("-- RESULT ---> CAN ACTIVATE", result);
     return result;
   }
 
   canAccessObs(): Promise<boolean> {
     return new Promise((resolve) => {
-      this.store.pipe(select(usuarioSelectors.userLoaded))
-      .pipe(
-        take(1),
-        tap(resp => console.log("GUARD USER LOADED:", resp)),
-      )
-      .subscribe((userLoaded) => {
-        if(!userLoaded) {
-          this.store.dispatch(usuarioActions.GET_USUARIO_ID());
-        }
-        else {
-          resolve(userLoaded)
-        }
-      });
 
-      this.store.pipe(select(usuarioSelectors.usuario)).pipe(
-        skip(1),
-        take(1),
-        tap(resp => console.log("USUARIO$ VALUE:", resp))
-      )
-      .subscribe((usuario) => {
-        resolve(!!usuario.id);
-      });
+      this.store.pipe(select(usuarioSelectors.userLoaded))
+        .pipe(
+          take(1),
+        )
+        .subscribe((userLoaded) => {
+          if (!userLoaded) {
+            this.store.dispatch(usuarioActions.GET_USUARIO_ID());
+
+            race(
+              this.store.pipe(select(usuarioSelectors.usuario))
+                .pipe(
+                  skip(1),
+                  take(1),
+                  map((usuario) => !!usuario.id)
+                ),
+              timer(5000)
+                .pipe(
+                  mapTo(false)
+                )
+            )
+              .subscribe((value) => {
+                resolve(value);
+              });
+
+
+
+          }
+          else {
+            resolve(userLoaded);
+          }
+        });
+
     });
   }
 }
