c356380f105795e8aaebff6caf877a67915c9b63
Rafael Estrada <ntaticat@gmail.com>
2023-03-11 01:26:32 -0600

    update both login and signup pages

--- src/app/modules/auth/signup-page/signup-page.component.ts
+++ src/app/modules/auth/signup-page/signup-page.component.ts
@@ -1,15 +1,63 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  UntypedFormBuilder,
+  UntypedFormGroup,
+  Validators,
+} from '@angular/forms';
+import {
+  faArrowRight,
+  faArrowLeft,
+  faCog,
+} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-signup-page',
   templateUrl: './signup-page.component.html',
-  styleUrls: ['./signup-page.component.scss']
+  styleUrls: ['./signup-page.component.scss'],
 })
 export class SignupPageComponent implements OnInit {
+  faArrowRight = faArrowRight;
+  faArrowLeft = faArrowLeft;
+  faCog = faCog;
 
-  constructor() { }
+  signUpForm: UntypedFormGroup;
 
-  ngOnInit(): void {
+  formEnable: boolean = true;
+  formIndex: number = 0;
+
+  constructor(private fb: UntypedFormBuilder) {
+    this.signUpForm = this.fb.group({
+      name: ['', Validators.required],
+      username: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+      samePassword: ['', [Validators.required]],
+    });
   }
 
+  ngOnInit(): void {}
+
+  onClickNextForm() {
+    this.formIndex = this.formIndex + 1;
+  }
+
+  onSubmitSignUp() {
+    if (this.signUpForm.valid) {
+      const name = this.signUpForm.get('name')?.value;
+      const username = this.signUpForm.get('username')?.value;
+      const email = this.signUpForm.get('email')?.value;
+      const password = this.signUpForm.get('password')?.value;
+
+      const userPost = {
+        name,
+        username,
+        email,
+        password,
+      };
+
+      // this.store.dispatch(usuarioActions.POST_LOGIN({ payload: {...credentials} }));
+    }
+  }
 }
