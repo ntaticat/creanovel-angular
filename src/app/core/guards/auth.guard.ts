4f466ae3826d6f3e16b51c4204c778cadac77154
Rafael Estrada <ntaticat@gmail.com>
2023-12-10 23:02:01 -0600

    upgrade angular from 15 to 16

--- src/app/core/guards/auth.guard.ts
+++ src/app/core/guards/auth.guard.ts
@@ -1,14 +1,5 @@
 import { Injectable } from '@angular/core';
-import {
-  ActivatedRouteSnapshot,
-  CanActivate,
-  CanLoad,
-  Route,
-  Router,
-  RouterStateSnapshot,
-  UrlSegment,
-  UrlTree,
-} from '@angular/router';
+import { ActivatedRouteSnapshot, Route, Router, RouterStateSnapshot, UrlSegment, UrlTree } from '@angular/router';
 import {
   Observable,
   Observer,
@@ -22,7 +13,7 @@ import { first, map, mapTo, skip, take, takeLast, tap } from 'rxjs/operators';
 @Injectable({
   providedIn: 'root',
 })
-export class AuthGuard implements CanActivate, CanLoad {
+export class AuthGuard  {
   constructor(private router: Router) {}
 
   async canLoad(route: Route, segments: UrlSegment[]) {
